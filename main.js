55731857d01d207c333947ffe5258833ae75a69c
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-21 19:53:24 +0600

    added decrement and increment

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ function increment(e){
        return getTheCount.innerText=10;
 }
 }
+function decrement(e){
+    let count=0;
+    const getTheCount=(e.parentNode.parentNode.childNodes[3]);
+    const getTheCountField=(e.parentNode.parentNode.childNodes[3].innerText)
+    const getTheValue=parseInt(getTheCountField);
+    count =getTheValue+1;
+    getTheCount.innerText=count;
+    if(getTheValue===10){
+       return getTheCount.innerText=10;
+}
+}
+
